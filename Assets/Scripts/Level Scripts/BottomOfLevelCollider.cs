f0f415c7e31831b6538d9edb64a153fe9d97dbed
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-05-21 16:33:28 +0200

    added auto respawn when player falls true level

---
+++ Assets/Scripts/Level Scripts/BottomOfLevelCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BottomOfLevelCollider : MonoBehaviour
+{
+    public Collider2D collider;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
