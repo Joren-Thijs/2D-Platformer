a6e233583da18836deee554ffa8531b38abcc9fe
Tim Blokken <tim_blokken@hotmail.com>
2020-05-22 18:00:53 +0200

    added gems and score UI

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+
+    public ScoreDisplay display;
+    public Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreDisplay>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            display.AddScore(1);
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
